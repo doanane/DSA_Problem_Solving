de37b12073d612c4f3e91f104a1d749d6854182c
doanane <anane365221@gmail.com>
2026-02-17 21:51:19 +0000

    solved uppercase and lowercase problem question

---
+++ case_difference.py
@@ -0,0 +1,9 @@
+def solution(typedText):
+    upper_count = 0
+    lower_count = 0
+    for ch in typedText:
+        if ch.isupper():
+            upper_count +=1
+        elif ch.islower():
+            lower_count +=1
+    return upper_count - lower_count
